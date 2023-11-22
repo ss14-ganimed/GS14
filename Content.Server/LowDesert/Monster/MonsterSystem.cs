46339f7fedcbcedab16b44468246cf858659270d
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2023-11-22 03:04:45 +0300

    Update MonsterSystem.cs

--- Content.Server/LowDesert/Monster/MonsterSystem.cs
+++ Content.Server/LowDesert/Monster/MonsterSystem.cs
@@ -4,6 +4,8 @@ using Content.Shared.Damage.Systems;
 using Content.Shared.DoAfter;
 using Content.Shared.LowDesert.Monster;
 using Content.Shared.LowDesert.Monster.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Systems;
 
 namespace Content.Server.LowDesert.Monster;
 
@@ -11,6 +13,7 @@ public sealed class MonsterSystem : SharedMonsterConsumeSystem
 {
 	
 	[Dependency] private readonly SharedActionsSystem _actions = default!;
+	[Dependency] private readonly MobThresholdSystem _thresholds = default!;
 	[Dependency] private readonly DamageableSystem _damage = default!;
 	
 	public override void Initialize()
@@ -44,16 +47,29 @@ public sealed class MonsterSystem : SharedMonsterConsumeSystem
 		if (!consumable.IsConsumable)
 			return;
 		
-		component.EvoPoints += consumable.EvoPointsGranted;
 		consumable.IsConsumable = false;
 		
 		if (EntityManager.TryGetComponent<DamageableComponent>(args.Args.Target, out var damageable))
 		{
+			if (_thresholds.TryGetThresholdForState(args.Args.Target.Value, MobState.Dead, out var threshold) && threshold is not null && threshold > 0.0f)
+			{
+				component.EvoPoints += consumable.EvoPointsGranted * (component.ConsumeDamageValue / (float)(threshold.Value));
+			}
+			else
+			{
+				component.EvoPoints += consumable.EvoPointsGranted;
+			}
+			
 			DamageSpecifier damage = new()
 			{
 				DamageDict = new() { { component.ConsumeDamageID, component.ConsumeDamageValue } }
 			};
 			_damage.TryChangeDamage(args.Args.Target, damage, true, true, damageable, uid);
+			
+		}
+		else
+		{
+			component.EvoPoints += consumable.EvoPointsGranted;
 		}
 	}
 	
