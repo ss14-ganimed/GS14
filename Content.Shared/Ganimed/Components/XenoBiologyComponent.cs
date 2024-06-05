7340cc810923b16df0299fb6b887f029355f85f0
Gorox221 <139872389+Gorox221@users.noreply.github.com>
2024-06-05 11:38:24 +0300

    Ксено

---
+++ Content.Shared/Ganimed/Components/XenoBiologyComponent.cs
@@ -0,0 +1,21 @@
+/// Maded by Gorox for Enterprise. See CLA
+using Content.Server.XenoBiology.Systems;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Dictionary;
+
+namespace Content.Shared.XenoBiology.Components;
+
+[RegisterComponent, Access(typeof(XenoBiologySystem))]
+public sealed partial class XenoBiologyComponent : Component
+{
+    [DataField("points"), ViewVariables(VVAccess.ReadWrite)]
+    public int Points = 0;
+
+    [DataField("mutagen", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string Mutagen = "MobSlimesPet";
+
+    [DataField("antimutagen", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string Antimutagen = "MobAdultSlimesGreen";
+}
